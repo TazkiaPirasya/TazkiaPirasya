65fbaeeeada2a93b700c0c23b7f5a03847f0c1f3
TazkiaPirasya <nonashofiaulia@gmail.com>
2026-05-04 09:20:39 +0700

    Add files via upload

---
+++ PRAKTIK SISTEM OUT.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.print("SMK YAPPRI");
+        System.out.println(" SUMEDANG");
+        System.out.print("nama= Ai Nurlita Zahra, ");
+        System.out.println("Tazkia Pirasya");
+        System.out.println("kelas= XI RPL-1");
+        System.out.println("jurusan= Teknik Informatika");
+        System.out.println("---------------------------------------");
+        System.out.print("Alamat:");
+         System.out.println("Jl.Wado-Kirisik KM.06 ");
+        System.out.println("Banjarsari-Jatitunggal Kab.Sumedang");
+        System.out.println("---------------------------------------");
+    }
+}
---
+++ PRAKTIK SISTEM VARIABEL.java
@@ -0,0 +1,38 @@
+public class Praktikvariabel {
+    public static void main(String[] args) {
+        String Nama="Tazkia Pirasya";
+        String hoby="listening music";
+        int umur=17;
+        double IQ=98.5;
+        boolean Status=true;
+        double Nilai=99.5;
+        
+        System.out.println("---Curriculum Vitae---");
+        System.out.println("Nama :" + Nama );
+        System.out.println("umur :" + umur );
+        System.out.println("hoby:" + hoby);
+        System.out.println("NILAI IQ:" + IQ);
+        System.out.println("Status Lulus:" + Status);
+        
+    
+        System.out.println("update nilai baru:"+ Nilai );
+        Ststem.out.println("--------------------------");
+        
+        String Nama1="Ai Nurlita Zahra";
+        String hoby1="sare";
+        int umur=17;
+        double IQ=98.5;
+        boolean Status=true;
+        double Nilai=99.5;
+        
+        System.out.println("---Curriculum Vitae---");
+        System.out.println("Nama :" + Nama );
+        System.out.println("umur :" + umur );
+        System.out.println("hoby:" + hoby);
+        System.out.println("NILAI IQ:" + IQ);
+        System.out.println("Status Lulus:" + Status);
+        
+    
+        System.out.println("update nilai baru:"+ Nilai );
+    }
+}
